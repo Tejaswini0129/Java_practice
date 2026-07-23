cf9c70386a5b8f3db0c8f2339b8b80f4daf5628e
Tejaswini0129 <tejaswinimedipelli@gmail.com>
2026-07-23 11:56:49 +0530

    core java

--- Java practice/CORE_JAVA/src/Package1/Demo.java
+++ Java practice/CORE_JAVA/src/Package1/Demo.java
@@ -1,8 +1,19 @@
 package Package1;
 
 public class Demo {
-   public static void main(String [] args) {
-	   System.out.println("Hii everyone this is Tejaswini");
-	   
-   }
-}
+	
+	     public boolean isPrime(int num){
+	        int count =0;
+	        for(int i = 1;i<=num;i++){
+	            if(num%i == 10){
+	                count++;
+	            }
+	        }
+	        return count ==2;
+	    }
+	    public static void main(String [] args){
+	        Demo obj = new Demo();
+	        System.out.println(obj.isPrime(2));
+	    }
+	}
+
